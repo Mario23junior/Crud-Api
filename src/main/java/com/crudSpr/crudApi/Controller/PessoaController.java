15457d195d52a9379dc28f1a79060596fed3e697
Mario23junior <mariojunior3251@gmail.com>
2020-07-18 17:36:58 -0300

    Listando dados por id

--- src/main/java/com/crudSpr/crudApi/Controller/PessoaController.java
+++ src/main/java/com/crudSpr/crudApi/Controller/PessoaController.java
@@ -1,8 +1,12 @@
 package com.crudSpr.crudApi.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,4 +26,21 @@ public class PessoaController {
 		return pessoaRespository.findAll();
 	}
 	
+	//Listando dados por id 
+	@RequestMapping(value = "/pessoa/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Pessoa> GetById(@PathVariable(value = "id") long id){
+		
+		Optional<Pessoa> pessoa = pessoaRespository.findById(id);
+		 if(pessoa.isPresent()) {
+			 return new ResponseEntity<Pessoa>(pessoa.get(), HttpStatus.OK);
+		 }else {
+			 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		 }
+ 	}
+	
 }
+
+
+
+
+
